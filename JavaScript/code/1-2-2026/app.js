ae07e1b72e88d485098ea565f2fb207f77dc4fbb
Sami123d <saminazirqureshi1@gmail.com>
2026-02-01 21:14:18 +0500

    first js class

---
+++ JavaScript/code/1-2-2026/app.js
@@ -0,0 +1,8 @@
+alert("Thanks for your input!");
+alert("Thanks for your input2!");
+
+var myName = "Sami Ahmed lorem Lorem ipsum dolor sit amet consectetu";
+console.log(myName)
+
+var age;
+console.log(age)
