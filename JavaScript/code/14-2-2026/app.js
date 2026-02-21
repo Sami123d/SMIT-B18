cc4e32f2bbded91279ec49dcf59fd7eb8a4feb74
Sami123d <saminazirqureshi1@gmail.com>
2026-02-21 17:05:01 +0500

    variable class

--- JavaScript/code/14-2-2026/app.js
+++ JavaScript/code/14-2-2026/app.js
@@ -26,7 +26,8 @@
 var num = 10;
  
 console.log(num++);
-console.log(++num);
+console.log(num)
+console.log(--num);
 console.log(num)
 // var num = 10;
 
---
+++ JavaScript/code/21-2-2026/app.js
@@ -0,0 +1,28 @@
+// es5 variable
+var sirName; //INITIALIZE
+var sirName = "sami";//declare
+
+console.log(sirName)
+// alert("welcome to saylani");
+sirName = "jaffar"  //value re-assign krdi
+var sirName = "Ahmed"; //redeclare and value change
+console.log(sirName);
+
+//es6 let, const 
+// let ==>initialize kr skta hun,redecalre nhi kr skte, value change ho skti hai
+
+let name;
+
+console.log(name);
+
+name = "jaffar";
+console.log(name);
+
+// const ayanPass;
+const ayanPass = "Admin123";
+console.log(ayanPass)
+ayanPass = "ayan123"
+
+
+
+
