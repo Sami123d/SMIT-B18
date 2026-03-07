af5c4bc797532830c35cc9be155301aff94d8fee
Sami123d <saminazirqureshi1@gmail.com>
2026-03-07 17:07:18 +0500

    data types in js

---
+++ JavaScript/code/07-03-2026/app.js
@@ -0,0 +1,40 @@
+
+// ******15 challenge*****
+// let score1 = 20;
+
+// let score2 = 10;
+
+// let score3 = 50;
+
+// let totalScore = score1 + score2 + score3;
+// console.log(totalScore, "total score")
+
+// let averageScore = totalScore / 3;
+// console.log(averageScore, "average score");
+
+// let plates = 20;
+// let people = 7;
+
+// let remainingPlate = plates % people;
+// console.log(remainingPlate, "remaining palte");
+
+// remainingPlate = remainingPlate + 1
+// console.log(remainingPlate)
+
+// let message = 'there are ' + remainingPlate + ' plates available'
+
+// console.log(message)
+
+
+// DATA TYPES
+
+let text = "sami ahmed";
+
+let number = 120;
+
+let isDoorOpen = false;
+
+let name = null;
+
+
+console.log(typeof(name))
